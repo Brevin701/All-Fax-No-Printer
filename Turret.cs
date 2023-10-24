8256d9bd6da6f654843045f4725095997af25dbd
Brevin701 <144558770+Brevin701@users.noreply.github.com>
2023-10-24 09:30:20 -0500

    Update Turret.cs

--- Turret.cs
+++ Turret.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private Transform target;
+
+    private voice Update() {
+        if (target == null){
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange()){
+            target = null;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FindTarget(){
+        RaycastHit2d[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)
+        transform.position, 0f, enemyMask);
+
+        if (hits.Length > 0) {
+            target = hits[0].transform;
+        }
     }
+
+    private bool CheckTargetIsInRange() {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+
+    private void RotateTowardsTarget() {
+        float angle = Mathf.Atan(target.position.y - tranform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private voice OnDrawGizmosSelected() {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+
+    }
+
 }
