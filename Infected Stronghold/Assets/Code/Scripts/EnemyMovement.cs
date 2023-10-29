4cfd9bab52a8a69a0b378cfe3c4e5f69e623ed85
Agustin <agustinmm7112@gmail.com>
2023-10-29 01:07:59 -0500

    FreeOption Added to Menu
    Freezes the zombies to slow down

--- Infected Stronghold/Assets/Code/Scripts/EnemyMovement.cs
+++ Infected Stronghold/Assets/Code/Scripts/EnemyMovement.cs
@@ -2,46 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
-{
-    [Header("Reference")]
-    [SerializeField] private Rigidbody2D rb;
+public class EnemyMovement : MonoBehaviour {
+    
+	[Header("References")]
+	[SerializeField] private Rigidbody2D rb;
 
-    [Header("Attributes")]
-    [SerializeField] private float moveSpeed = 2f;
+	[Header("Attributes")]
+	[SerializeField] private float moveSpeed = 2f;
 
-    private Transform target;
-    private int pathIndex = 0;
-    // Start is called before the first frame update
-    private void Start()
-    {
-        target = LevelManager.main.path[pathIndex];
-    }
+	private Transform target;
+	private int pathIndex = 0;
 
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
-        {
-            pathIndex++;
+	private float baseSpeed;
 
+	private void Start() {
+		baseSpeed = moveSpeed;
+		target = LevelManager.main.path[pathIndex];
+	}
 
-            if (pathIndex == LevelManager.main.path.Length)
-            {
-                EnermySpawner.onEnemyDestroy.Invoke();
-                Destroy(gameObject);
-                return;
-            }
-            else
-            {
-                target = LevelManager.main.path[pathIndex];
-            }
-        }
-    }
-    private void FixedUpdate()
-    {
-        Vector2 direction = (target.position - transform.position).normalized;
+	private void Update() {
+		if (Vector2.Distance(target.position, transform.position) <= 0.1f) {
+			pathIndex++;
 
-        rb.velocity = direction * moveSpeed;
-    }
+			if (pathIndex == LevelManager.main.path.Length) {
+				EnermySpawner.onEnemyDestroy.Invoke();
+				Destroy(gameObject);
+				return;
+			} else {
+				target = LevelManager.main.path[pathIndex];
+			}
+		}
+	}
+
+	private void FixedUpdate() {
+		Vector2 direction = (target.position - transform.position).normalized;
+
+		rb.velocity = direction * moveSpeed;
+	}
+
+	public void UpdateSpeed(float newSpeed) {
+		moveSpeed = newSpeed;
+	}
+
+	public void ResetSpeed() {
+		moveSpeed = baseSpeed;
+	}
+
 }
