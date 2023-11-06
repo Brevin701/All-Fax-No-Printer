7be2312b3c6e69b99169fde54df1857624af8ae0
Griffin2108 <143625479+Griffin2108@users.noreply.github.com>
2023-11-06 01:06:34 -0500

    I hope this fixes issues

--- Infected Stronghold/Assets/Code/Scripts/LevelManager.cs
+++ Infected Stronghold/Assets/Code/Scripts/LevelManager.cs
@@ -16,24 +16,26 @@ public class LevelManager : MonoBehaviour
         main = this;
     }
 
-    private void Start() 
+    private void Start()
     {
         currency = 100;
     }
 
-    public void IncreaseCurrency(int amount) 
+    public void IncreaseCurrency(int amount)
     {
         currency += amount;
     }
 
     public bool SpendCurrency(int amount)
     {
-        if (amount <= currency) {
-            //BUY ITEM
-            currency -= amount;
+        if (amount <= currency)
+        {
+           currency -= amount;
             return true;
-        } else{
-            Debug.Log ("You dont got the bread for that brother");
+        }
+        else
+        {
+            Debug.Log("You do not have enough to purchase this item");
             return false;
         }
     }
